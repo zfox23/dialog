bf9393b5dabd45d68227f8973bd2970a905fdd9e
Zachary Fox <valefox23@gmail.com>
2023-08-01 14:28:08 -0600

    Realize GitHub Pages doesn't support deploying from a subfolder and delete public

--- hubs-webrtc-tester/app/public/component---src-pages-404-tsx-76424607f1259c30f4b9.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkhubs_webrtc_tester=self.webpackChunkhubs_webrtc_tester||[]).push([[218],{2513:function(e,t,r){r.r(t);var u=r(7294),l=r(151);t.default=e=>{let{data:t}=e;return u.createElement(l.A,null,u.createElement("p",null,"404"))}}}]);
-//# sourceMappingURL=component---src-pages-404-tsx-76424607f1259c30f4b9.js.map
